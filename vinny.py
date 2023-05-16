de068c9214ae1bd56c0afa9fd65481e8af76c90f
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-05-16 14:50:18 +0530

    Update vinny.py

--- vinny.py
+++ vinny.py
@@ -1,39 +1,38 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
 
-# Load pre-trained model and tokenizer
-model_name = "microsoft/DialoGPT-medium"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+class ConversationalSentimentAnalyzer:
+    def __init__(self, model_name):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Define the sentiment analysis function
-def get_conversation_sentiment(conversation):
-    # Encode the conversation
-    encoded_input = tokenizer.encode(conversation, return_tensors="pt")
+    def analyze_sentiment(self, conversation):
+        try:
+            # Tokenize the conversation
+            inputs = self.tokenizer.encode_plus(
+                conversation,
+                add_special_tokens=True,
+                padding="longest",
+                truncation=True,
+                return_tensors="pt"
+            )
+            inputs = inputs.to(self.device)
 
-    # Generate response from the model
-    with torch.no_grad():
-        generated = model.generate(
-            encoded_input.input_ids,
-            max_length=100,
-            num_return_sequences=1,
-            no_repeat_ngram_size=2,
-            pad_token_id=tokenizer.eos_token_id,
-            do_sample=True,
-            top_k=50,
-            top_p=0.95,
-            temperature=0.7,
-        )
+            # Perform sentiment analysis
+            with torch.no_grad():
+                logits = self.model(**inputs).logits
+                predicted_labels = torch.argmax(logits, dim=1)
+                sentiment_label = "Positive" if predicted_labels.item() == 1 else "Negative"
+            
+            return sentiment_label
 
-    # Decode and extract sentiment
-    generated_text = tokenizer.decode(generated[0], skip_special_tokens=True)
-    sentiment = get_sentiment(generated_text)  # You need to implement the sentiment analysis function
+        except Exception as e:
+            print("Error analyzing sentiment:", e)
+            return None
 
-    return sentiment
-
-# Example conversation
+# Example usage
 conversation = "User: How are you?\nBot: I'm doing great! How about you?\nUser: I'm feeling happy today."
-
-# Get sentiment of the conversation
-sentiment = get_conversation_sentiment(conversation)
+analyzer = ConversationalSentimentAnalyzer("bert-base-uncased")
+sentiment = analyzer.analyze_sentiment(conversation)
 print("Sentiment:", sentiment)
