4e0ddd217cfb26e6786281db8b597b9864cfd945
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-05-16 15:47:45 +0530

    Update vinny.py

--- vinny.py
+++ vinny.py
@@ -1,39 +1,53 @@
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
 
-def get_sentiment(text):
-    sid = SentimentIntensityAnalyzer()
-    sentiment_scores = sid.polarity_scores(text)
-    sentiment_label = 'Positive' if sentiment_scores['compound'] >= 0 else 'Negative'
-    return sentiment_label
+# Load pre-trained BERT model and tokenizer
+model_name = 'bert-base-uncased'
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertForSequenceClassification.from_pretrained(model_name, num_labels=3)  # Set num_labels to 3 for positive, negative, neutral
 
+# Set device (GPU if available, else CPU)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
+
+# Example conversation data
 conversation = [
     "User: How are you?",
     "Bot: I'm doing great! How about you?",
     "User: I'm feeling happy today"
 ]
 
-# Analyze sentiment for each message in the conversation
-sentiments = []
+# Process conversation data and prepare input for model
+inputs = []
 for message in conversation:
-    # Extract the speaker and text from the message
-    speaker, text = message.split(":")
-    text = text.strip()
-    
-    # Determine the sentiment label for the message
-    sentiment = get_sentiment(text)
-    
-    # Assign sentiment label to the message
-    labeled_message = f"{speaker}: {text} ({sentiment})"
-    sentiments.append(labeled_message)
+    # Extract the text from the message (excluding the speaker prefix)
+    text = message.split(":")[1].strip()
+    inputs.append(text)
 
-# Print the sentiment labels for each message
-for message in sentiments:
-    print(message)
+# Tokenize and encode the input
+encoded_input = tokenizer.batch_encode_plus(
+    inputs,
+    truncation=True,
+    padding=True,
+    max_length=128,
+    return_tensors='pt'
+)
 
-# Determine the overall sentiment of the conversation
-positive_count = sum(1 for message in sentiments if 'Positive' in message)
-negative_count = sum(1 for message in sentiments if 'Negative' in message)
-overall_sentiment = 'Positive' if positive_count > negative_count else 'Negative'
+# Move input tensors to the device
+input_ids = encoded_input['input_ids'].to(device)
+attention_mask = encoded_input['attention_mask'].to(device)
 
-print("Overall sentiment:", overall_sentiment)
+# Perform inference
+model.eval()
+with torch.no_grad():
+    outputs = model(input_ids, attention_mask=attention_mask)
+    logits = outputs.logits
+    predicted_labels = torch.argmax(logits, dim=1).cpu().numpy()
+
+# Map predicted labels to sentiment labels
+sentiment_labels = ["Negative", "Neutral", "Positive"]
+sentiments = [sentiment_labels[label] for label in predicted_labels]
+
+# Print sentiment labels for each message in the conversation
+for message, sentiment in zip(conversation, sentiments):
+    print(f"{message} ({sentiment})")
