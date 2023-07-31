d9ee6727cec87fdcb54310a4b3799fc223353ddb
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-08-01 00:30:37 +0530

    Update vinny1.py

--- vinny1.py
+++ vinny1.py
@@ -3,32 +3,33 @@ import requests
 def get_node_exporter_metrics(url):
     try:
         response = requests.get(url)
-        if response.status_code == 200:
-            return response.text
-        else:
-            print(f"Failed to fetch metrics. Status code: {response.status_code}")
-            return None
+        response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+        return response.text
     except requests.exceptions.RequestException as e:
         print(f"Error occurred while fetching metrics: {e}")
         return None
 
 def calculate_cpu_usage(metrics_text):
-    cpu_metrics = [line for line in metrics_text.splitlines() if line.startswith('cpu ')]
-    if not cpu_metrics:
+    try:
+        cpu_metrics = [line for line in metrics_text.splitlines() if line.startswith('cpu ')]
+        if not cpu_metrics:
+            raise ValueError("Failed to find 'cpu' metrics in the response.")
+        
+        # Parse the CPU metrics
+        cpu_metrics = cpu_metrics[0].split()[1:]
+
+        # Calculate total CPU time
+        total_cpu_time = sum(map(int, cpu_metrics))
+
+        # Calculate idle CPU time
+        idle_cpu_time = int(cpu_metrics[3])
+
+        # Calculate CPU usage percentage
+        cpu_usage_percentage = 100.0 * (1.0 - idle_cpu_time / total_cpu_time)
+        return cpu_usage_percentage
+    except (IndexError, ValueError) as e:
+        print(f"Error occurred while calculating CPU usage: {e}")
         return None
-
-    # Parse the CPU metrics
-    cpu_metrics = cpu_metrics[0].split()[1:]
-
-    # Calculate total CPU time
-    total_cpu_time = sum(map(int, cpu_metrics))
-
-    # Calculate idle CPU time
-    idle_cpu_time = int(cpu_metrics[3])
-
-    # Calculate CPU usage percentage
-    cpu_usage_percentage = 100.0 * (1.0 - idle_cpu_time / total_cpu_time)
-    return cpu_usage_percentage
 
 if __name__ == "__main__":
     prometheus_node_exporter_url = "http://localhost:9001/metrics"
