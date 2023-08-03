b6199c8a7cd1196aff7ec4ea7064b00e7f31ec66
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-08-03 17:19:57 +0530

    Update vinny.py

--- vinny.py
+++ vinny.py
@@ -1,7 +1,14 @@
-def push_result_to_DB(df):
+
+import pandas as pd
+from sqlalchemy import create_engine
+import urllib
+
+def fetch_data_from_db():
     password = "merck@1234"
     encoded_password = urllib.parse.quote(password, safe='')
     engine = create_engine(f"postgresql://merck_svc:{encoded_password}@localhost/merck_db")
-    record_ins=df.to_sql('prediction_result1', con=engine, if_exists='append', index=False)
-    print("records pushes to db is",record_ins)
-    return record_ins  
+    
+    query = "SELECT * FROM prediction_result1"
+    df = pd.read_sql_query(query, engine)
+
+    return df
