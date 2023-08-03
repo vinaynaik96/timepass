28649fc7227f450938485a7f82a62e952297b174
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-08-03 16:56:06 +0530

    Update vinny.py

--- vinny.py
+++ vinny.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import json
+import datetime
 
 def monitor_prometheus(threshold=30):
     prome_sql = """(sum by(instance) (irate(node_cpu_seconds_total{mode!="idle"}[1m])) / on(instance) group_left sum by (instance)((irate(node_cpu_seconds_total[1m])))) * 100"""
@@ -22,4 +23,8 @@ def send_alert(data):
     return f"The CPU Utilization is {data} and Alert Number : 123456"
     
 if __name__=="__main__":
+    # delay the start of the script until the next minute begins
+    now = datetime.datetime.now()
+    delay = 60 - now.second
+    time.sleep(delay)
     monitor_prometheus()
