424c450dc76bc8eb370162e9f8efcd82e6363f55
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-08-03 00:47:59 -1100

    Update vinny.py

--- vinny.py
+++ vinny.py
@@ -1,30 +1,7 @@
-import requests
-import time
-import json
-import datetime
-
-def monitor_prometheus(threshold=30):
-    prome_sql = """(sum by(instance) (irate(node_cpu_seconds_total{mode!="idle"}[1m])) / on(instance) group_left sum by (instance)((irate(node_cpu_seconds_total[1m])))) * 100"""
-    url= 'http://54.162.54.22:9090/api/v1/query'
-    params = {'query': prome_sql}
-    
-    while True:
-        try:
-            response = requests.get(url,params=params)
-            data = float(response.json()["data"]['result'][0]['value'][1])
-            print(f"current value: {data}")
-            if data > threshold:
-                print(send_alert(data))        
-        except Exception as e:
-            print(f"error: {e}")
-        time.sleep(60)
-
-def send_alert(data):
-    return f"The CPU Utilization is {data} and Alert Number : 123456"
-    
-if __name__=="__main__":
-    # delay the start of the script until the next minute begins
-    now = datetime.datetime.now()
-    delay = 60 - now.second
-    time.sleep(delay)
-    monitor_prometheus()
+def push_result_to_DB(df):
+    password = "merck@1234"
+    encoded_password = urllib.parse.quote(password, safe='')
+    engine = create_engine(f"postgresql://merck_svc:{encoded_password}@localhost/merck_db")
+    record_ins=df.to_sql('prediction_result1', con=engine, if_exists='append', index=False)
+    print("records pushes to db is",record_ins)
+    return record_ins  
