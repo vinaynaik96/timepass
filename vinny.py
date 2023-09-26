41bedaf77bc4576c734948380e3ec191367e7f75
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-09-26 23:44:47 +0530

    Update vinny.py

--- vinny.py
+++ vinny.py
@@ -1,4 +1,21 @@
-os.environ["OPENAI_API_TYPE"] = "azure"
-os.environ["OPENAI_API_VERSION"] = "2023-05-15"
-os.environ["OPENAI_API_BASE"] = "https://azureopenaicoe.openai.azure.com/"
-os.environ["OPENAI_API_KEY"] = "ceea3c2ec4814ed89507f4ee06b907a2"
+from langchain.chat_models import AzureChatOpenAI
+from langchain.schema import HumanMessage
+
+BASE_URL = "https://azureopenaicoe.openai.azure.com/"
+API_KEY = "ceea3c2ec4814ed89507f4ee06b907a2"
+DEPLOYMENT_NAME = "chat"
+model = AzureChatOpenAI(
+    openai_api_base=BASE_URL,
+    openai_api_version="2023-05-15",
+    deployment_name=DEPLOYMENT_NAME,
+    openai_api_key=API_KEY,
+    openai_api_type="azure",
+)
+
+model(
+    [
+        HumanMessage(
+            content="create a python code which sum two argument"
+        )
+    ]
+)
