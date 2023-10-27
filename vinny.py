80de4f25975e49e59c42b7352fd965df37abc151
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-10-27 15:36:27 +0530

    Update vinny.py

--- vinny.py
+++ vinny.py
@@ -1,78 +1,22 @@
-from langchain.chat_models import AzureChatOpenAI
-from langchain.schema import SystemMessage, HumanMessage
-import streamlit as st
- 
-BASE_URL = "https://azureopenaicoe.openai.azure.com/"
-API_KEY = "ceea3c2ec4814ed89507f4ee06b907a2"
-DEPLOYMENT_NAME = "chat"
- 
-model = AzureChatOpenAI(
-    openai_api_base=BASE_URL,
-    openai_api_version="2023-05-15",
-    deployment_name=DEPLOYMENT_NAME,
-    openai_api_key=API_KEY,
-    openai_api_type="azure",
-)
+Create a Technical Knowledge Base Article
 
+**Objective:**
+Provide a clear and concise objective for the article.
 
-import streamlit as st
-import os
+**Procedure:**
+1. Describe the problem or topic that the article addresses.
+2. Provide step-by-step instructions to solve the problem or explore the topic.
+3. Include any code snippets, commands, or configuration details where relevant.
+4. Use bullet points or numbered lists for clarity.
+5. Add screenshots or diagrams to illustrate key points.
 
-# App title
-st.set_page_config(page_title="💬 Knowledge Base Chatbot")
+**Conclusion:**
+Summarize the key takeaways or results of following the procedure.
+Highlight any important considerations or potential pitfalls.
 
-st.title('💬 Knowledge Base Chatbot')
+**Authorship:**
+Include the name of the author and the date of creation at the end of the article.
 
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+[Author Name]
+[Date]
 
-# Display or clear chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-
-
-def generate_response(prompt_input,user_input):
-    system_message = SystemMessage(content=f"create a technical knowledge article create objective as Heading,procedure and conclusion inside that it should keep these content without much change and at end add created by whom and date {prompt_input}")
-    user_message = HumanMessage(content=user_input)
-    messages = [system_message, user_message]
-    response = model(messages)
-    return response.content
-
-custom_css = '''
-    <style>
-        textarea.stTextArea {
-            width: 800px !important;
-            height: 400px !important;
-        }
-    </style>
-    '''
-
-st.write(custom_css, unsafe_allow_html=True)
-
-with st.sidebar:
-    st.title("Service Now Description")
-    user_input = st.text_area("Type your text here:")
-    st.subheader("Your Conversation :")
-    st.write(user_input)
-    
-# User-provided prompt
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
-
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
-
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = generate_response(prompt,user_input)
-            st.write(response)
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
