6bd25b0a89a390a04488f4aa1cfd09705473f046
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-11-15 20:04:26 +0530

    Update vinny.py

--- vinny.py
+++ vinny.py
@@ -4,7 +4,7 @@ from langchain.chat_models import ChatOpenAI
 import streamlit as st
 import pandas as pd
 from langchain.chains import RetrievalQA
-from langchain.document_loaders import CSVLoader ,DataFrameLoader
+from langchain.document_loaders import CSVLoader, DataFrameLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import AzureOpenAI
 from langchain.vectorstores import Chroma
@@ -16,31 +16,24 @@ from langchain.prompts import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
-from langchain.chains import LLMChain
-from langchain.chat_models import AzureChatOpenAI
-# LLM
+from langchain.chains import LLMChain, ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 import os
-import openai
+
 os.environ["OPENAI_API_TYPE"] = "azure"
 os.environ["OPENAI_API_VERSION"] = "2023-05-15"
 os.environ["OPENAI_API_BASE"] = "https://azureopenaicoe.openai.azure.com/"
 os.environ["OPENAI_API_KEY"] = "ceea3c2ec4814ed89507f4ee06b907a2"
-llm=AzureOpenAI(deployment_name='langchain')
-from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
+
+llm = AzureOpenAI(deployment_name='langchain')
+
 
 def clear_submit():
-    """
-    Clear the Submit Button State
-    Returns:
-
-    """
     st.session_state["submit"] = False
 
-    
+
 def render_clickable_link(lst):
-    i=0
+    i = 0
     for dictionary in lst:
         st.write(f"{i}:")
         for key, value in dictionary.items():
@@ -49,16 +42,16 @@ def render_clickable_link(lst):
             else:
                 st.write(f"{key}: {value}")
         st.write(" ")
-        i=i+1
-            
+        i = i + 1
+
 
 memory = ConversationBufferMemory()
 chat_llm = AzureChatOpenAI(deployment_name='chat')
-# Prompt 
+# Prompt
 prompt = ChatPromptTemplate(
     messages=[
         SystemMessagePromptTemplate.from_template(
-            """You are a briliant code assistant give only valid code.
+            """You are a brilliant code assistant. Give only valid code.
             If you don't know the answer, just say that you don't know, don't try to make up an answer. 
             
             {question}
@@ -69,7 +62,7 @@ prompt = ChatPromptTemplate(
         HumanMessagePromptTemplate.from_template("{question}")
     ]
 )
-memory = ConversationBufferMemory(memory_key="chat_history",return_messages=True)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 conversation = LLMChain(
     llm=chat_llm,
@@ -82,12 +75,11 @@ st.set_page_config(page_title="Search The Bot Or Generate Your Own", page_icon="
 st.title("Search The Bot Or Generate Your Own")
 
 embeddings = OpenAIEmbeddings(deployment_name='text-embedding-ada-002')
-llm=AzureOpenAI(deployment_name='langchain')
+llm = AzureOpenAI(deployment_name='langchain')
 
 retriever = Chroma(persist_directory="./Botstabledata_db", embedding_function=embeddings).as_retriever()
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 qa = ConversationalRetrievalChain.from_llm(llm, retriever, memory=memory)
-
 
 if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
@@ -100,28 +92,27 @@ if "visibility" not in st.session_state:
 
 
 if prompt := st.chat_input(placeholder="Please Type Your Query"):
-    prompt_engg= prompt+" remember give me unique 3 bot names"
+    prompt_engg = prompt + " remember give me unique 3 bot names"
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    
+
     with st.chat_message("assistant"):
-            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-#            response = compression_retriever.get_relevant_documents(prompt_engg)
-            response = qa({"question": prompt_engg})
-            res=[]
-            res1=[]
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = qa({"question": prompt_engg})
+        res = []
+        res1 = []
+        print("===============")
+        print(type(response["answer"]))
+        print("===============")
+        for k, v in response.items():
+            print(k, v)
             print("===============")
-            print(type(response["answer"]))
-            print("===============")
-            for k, v in response.items():
-                print(k, v)
-                print("===============")
-            print(response["answer"])
-            dct={}
-            res.append(response["answer"])
-            res1.append(dct)
-            if len(res) ==0:
-                res="No Result Found , Please Give Valid Description"
-                st.write(res)
-            st.session_state.messages.append({"role": "assistant", "content": res})
+        print(response["answer"])
+        dct = {}
+        res.append(response["answer"])
+        res1.append(dct)
+        if len(res) == 0:
+            res = "No Result Found , Please Give Valid Description"
             st.write(res)
+        st.session_state.messages.append({"role": "assistant", "content": res})
+        st.write(res)
