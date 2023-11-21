c2c1a62dcc37fd7d9c984e0ed911967824fdfe8c
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-11-21 22:24:17 +0530

    Update vinny1.py

--- vinny1.py
+++ vinny1.py
@@ -45,27 +45,29 @@ retriever = Chroma(persist_directory="./Botstabledata_db", embedding_function=em
 compressor = LLMChainExtractor.from_llm(llm)
 compression_retriever = ContextualCompressionRetriever(base_compressor=compressor, base_retriever=retriever)
 
-if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+if "user_messages" not in st.session_state:
+    st.session_state.user_messages = []
 
-if "conversation_buffer" not in st.session_state:
-    st.session_state.conversation_buffer = []
+if "bot_responses" not in st.session_state:
+    st.session_state.bot_responses = []
 
-# Function to update conversation buffer memory
-def update_conversation_buffer(role, content):
-    st.session_state.conversation_buffer.append({"role": role, "content": content})
+def update_user_messages(content):
+    st.session_state.user_messages.append(content)
 
-# Display conversation history
-for msg in st.session_state.conversation_buffer:
-    st.chat_message(msg["role"]).write(msg["content"])
+def update_bot_responses(content):
+    st.session_state.bot_responses.append(content)
 
-# Inside the chat input block where you process user input
+for user_msg in st.session_state.user_messages:
+    st.chat_message("user").write(user_msg)
+
+for bot_resp in st.session_state.bot_responses:
+    st.chat_message("assistant").write(bot_resp)
+
 if prompt := st.chat_input(placeholder="Please Type Your Query"):
     prompt_engg = prompt + " remember give me unique 3 bot names"
-    update_conversation_buffer("user", prompt)  # Store user input in conversational buffer
+    update_user_messages(prompt)
     st.chat_message("user").write(prompt)
-    
-    # Assistant response handling
+
     with st.chat_message("assistant"):
         st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
         response = compression_retriever.get_relevant_documents(prompt_engg)
@@ -75,16 +77,15 @@ if prompt := st.chat_input(placeholder="Please Type Your Query"):
             dct = {}
             bot_name = data.metadata['bot_name']
             bot_name = bot_name.replace(" ", "")
-            bot_url = f"https://www.Botstore.com/botname={bot_name}"                
+            bot_url = f"https://www.Botstore.com/botname={bot_name}"
             dct["Description"] = data.page_content
             dct["BotName"] = data.metadata['bot_name']
-            dct["BotURL"] = bot_url              
+            dct["BotURL"] = bot_url
             output = f"Description : {data.page_content} \n BotName : {data.metadata['bot_name']}  \n BotURL : {bot_url}"
             res.append(output)
             res1.append(dct)
         if len(res) == 0:
             res = "No Result Found, Please Give Valid Description"
             st.write(res)
-        st.session_state.messages.append({"role": "assistant", "content": res})
-        update_conversation_buffer("assistant", res)  # Store assistant response in conversational buffer
+        update_bot_responses(res)
         render_clickable_link(res1)
