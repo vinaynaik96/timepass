8cf9174a97b279b7dc8d61fa367aa6fde6f4aeff
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-12-01 00:22:26 +0530

    Update vinny1.py

--- vinny1.py
+++ vinny1.py
@@ -1,65 +1,87 @@
-import streamlit as st
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders.generic import GenericLoader
-from langchain.document_loaders.parsers import LanguageParser
-from langchain.embeddings import AzureOpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chat_models import AzureChatOpenAI
-from langchain.memory import ConversationSummaryMemory
+from langchain.callbacks import StreamlitCallbackHandler
+import streamlit as st
+from langchain.callbacks.manager import CallbackManager
+import os
+from langchain.vectorstores import Chroma
+from langchain.embeddings import AzureOpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationTokenBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.text_splitter import Language
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-# Set up Langchain
-loader = GenericLoader.from_filesystem(
-    "./test_repo/langchain/libs/langchain",
-    glob="*",
-    suffixes=[".py", ".js"],
-    parser=LanguageParser(),
-)
-documents = loader.load()
+os.environ["OPENAI_API_TYPE"] = "azure"
+os.environ["OPENAI_API_VERSION"] = "2023-05-15"
+os.environ["OPENAI_API_KEY"] = "22c2ca1a04134562be1ab848aaba7d9c"
+os.environ["OPENAI_API_BASE"] = "https://coeaoai.openai.azure.com/"
 
-python_splitter = RecursiveCharacterTextSplitter.from_language(
-    language=Language.PYTHON, chunk_size=2000, chunk_overlap=200
-)
-texts = python_splitter.split_documents(documents)
-
+def clear_submit():
+    st.session_state["submit"] = False
+        
 embeddings = AzureOpenAIEmbeddings(azure_deployment='text-embedding-ada-002')
-
-db = Chroma.from_documents(texts, embedding=embeddings)
-retriever = db.as_retriever(search_type="mmr", search_kwargs={"k": 8})
 
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 llm = AzureChatOpenAI(deployment_name='chat', callback_manager=callback_manager, verbose=True)
+        
+def create_chain(llm, prompt, CONDENSE_QUESTION_PROMPT, db):
+    memory = ConversationTokenBufferMemory(llm=llm, memory_key="chat_history", return_messages=True, input_key='question', output_key='answer')
+    chain = ConversationalRetrievalChain.from_llm(llm=llm, chain_type="stuff", retriever=db.as_retriever(search_kwargs={"k": 3}), return_source_documents=True, max_tokens_limit=256, combine_docs_chain_kwargs={"prompt": prompt}, condense_question_prompt=CONDENSE_QUESTION_PROMPT, memory=memory)
+    return chain
 
-memory = ConversationSummaryMemory(llm=llm, memory_key="chat_history", return_messages=True)
-qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
+def set_custom_prompt_condense():
+    _template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+    Chat History:
+    {chat_history}
+    Follow Up Input: {question}
+    Standalone question:"""
+    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+    return CONDENSE_QUESTION_PROMPT
 
+def set_custom_prompt():
+    prompt_template = """Use the following pieces of information to answer the user's question.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Context: {context}
+    Question: {question}
+    Only return the helpful answer below and nothing else.
+    Helpful answer:
+    """
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    return prompt
 
-# Streamlit app
-def main():
-    st.title("Code Description & QA App")
+prompt = set_custom_prompt()
+CONDENSE_QUESTION_PROMPT = set_custom_prompt_condense()
 
-    uploaded_file = st.file_uploader("Upload a Python file (.py)", type="py")
+# Load from disk
+db = Chroma(persist_directory="Botstabledata_db", embedding_function=embeddings)
+qa = create_chain(llm=llm, prompt=prompt, CONDENSE_QUESTION_PROMPT=CONDENSE_QUESTION_PROMPT, db=db)
 
-    if uploaded_file is not None:
-        file_contents = uploaded_file.read().decode("utf-8")  # Get text content of uploaded file
+# Check session_state for existing conversation history or create a new one
+if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-        # Process the uploaded code file
-        question = "What is this code about?"  # Define a default question for description generation
-        result = qa(file_contents, user_question=question)
+st.title('🦜🔗 Q&A ChatBot')
 
-        # Display description and QA results
-        st.subheader("Description:")
-        st.write(result['description'])
+if prompt := st.text_input("Please Type Your Query"):
+    query = prompt + " remember give me unique 3 bot names"
+    st.session_state["messages"].append({"role": "user", "content": prompt})
 
-        st.subheader("QA Results:")
-        for qa_result in result['qa_results']:
-            st.write(f"Question: {qa_result['question']}")
-            st.write(f"Answer: {qa_result['answer']}")
-            st.write("-----")
+    with st.spinner("Thinking..."):
+        response = qa({"question": query})
+        count = 1
+        for data in response["source_documents"]:
+            dct = {}
+            bot_name = data.metadata['bot_name']
+            bot_name = bot_name.replace(" ", "")
+            bot_url = f"https://www.Botstore.com/botname={bot_name}"                
+            dct["Description"] = data.page_content
+            dct["BotName"] = data.metadata['bot_name']
+            dct["BotURL"] = bot_url
+            output = f"{count}) BotName : {data.metadata['bot_name']} \n\n Description : {data.page_content} \n\n BotURL : {bot_url}\n\n\n"
+            st.write(output)
+            count += 1
+        if len(response["source_documents"]) == 0:
+            st.write("No Result Found. Please provide a valid description.")
 
-
-if __name__ == "__main__":
-    main()
+        # Update session_state with assistant's response
+        st.session_state["messages"].append({"role": "assistant", "content": response})
