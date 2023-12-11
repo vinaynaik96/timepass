c5776c1ab3b2247a9b4ec07a6c0518df2b9b50ec
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-12-11 08:33:12 +0530

    Update vinny.py

--- vinny.py
+++ vinny.py
@@ -1,95 +1,28 @@
-import streamlit as st
-import tempfile
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders.generic import GenericLoader
-from langchain.document_loaders.parsers import LanguageParser
-from langchain.embeddings import AzureOpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.chat_models import AzureChatOpenAI
-from langchain.memory import ConversationSummaryMemory
-from langchain.chains import ConversationalRetrievalChain
-from langchain.text_splitter import Language
-from langchain.document_loaders import NotebookLoader, PythonLoader
-
 import os
+from fastapi import FastAPI
+import pandas as pd
 
-os.environ["OPENAI_API_TYPE"] = "azure"
-os.environ["OPENAI_API_VERSION"] = "2023-05-15"
-os.environ["OPENAI_API_KEY"] = "22c2ca1a04134562be1ab848aaba7d9c"
-os.environ["OPENAI_API_BASE"] = "https://coeaoai.openai.azure.com/"
+app = FastAPI()
 
-FILE_LOADER_MAPPING = {
-    "ipynb": (NotebookLoader, {}),
-    "py": (PythonLoader, {}),
-}
+def read_excel_from_directory(file_path):
+    if os.path.exists(file_path):
+        # Read the Excel file using pandas
+        df = pd.read_excel(file_path)
+        return df
+    else:
+        return None
 
-def create_vector_database(documents):
-    python_splitter = RecursiveCharacterTextSplitter.from_language(language=Language.PYTHON,
-                                                               chunk_size=2000,
-                                                               chunk_overlap=200)
-    texts = python_splitter.split_documents(documents)
-    embeddings = AzureOpenAIEmbeddings(azure_deployment='text-embedding-ada-002')
-    persist_directory = 'code_db'
-    # Create and persist a Chroma vector database from the chunked documents
-    db = Chroma.from_documents(
-        documents=texts,
-        embedding=embeddings,
-        #persist_directory=persist_directory
-        # persist_directory=DB_DIR,
-    )
-    #db.persist()
-
-    return db
-
-def run_code_rag():
-    st.title("Code Description")
-     # Upload files
-    uploaded_files = st.file_uploader("Upload your documents", type=[ "py", "ipynb"], accept_multiple_files=True)
-    loaded_documents = []
-
-    if uploaded_files:
-            # Create a temporary directory
-        with tempfile.TemporaryDirectory() as td:
-                # Move the uploaded files to the temporary directory and process them
-                for uploaded_file in uploaded_files:
-                    st.write(f"Uploaded: {uploaded_file.name}")
-                    ext = os.path.splitext(uploaded_file.name)[-1][1:].lower()
-                    st.write(f"Uploaded: {ext}")
+@app.get("/read_excel/")
+async def read_excel():
+    directory = "your_directory_path_here"  # Specify your directory path here
+    file_name = "your_excel_file.xlsx"  # Specify your Excel file name
     
-                    # Check if the extension is in FILE_LOADER_MAPPING
-                    if ext in FILE_LOADER_MAPPING:
-                        loader_class, loader_args = FILE_LOADER_MAPPING[ext]
-                        # st.write(f"loader_class: {loader_class}")
+    excel_file_path = os.path.join(directory, file_name)
+    data = read_excel_from_directory(excel_file_path)
     
-                        # Save the uploaded file to the temporary directory
-                        file_path = os.path.join(td, uploaded_file.name)
-                        with open(file_path, 'wb') as temp_file:
-                            temp_file.write(uploaded_file.read())
-    
-                        # Use Langchain loader to process the file
-                        loader = GenericLoader.from_filesystem("/home/ubuntu/vinay/llm/test_rag",glob="*",suffixes=[".py", ".js"],
-                                     parser=LanguageParser(),)
-                        documents = loader.load()
-                        #loaded_documents.extend(loader.load())
-                    else:
-                        st.warning(f"Unsupported file extension: {ext}")
-                        
-        #persist_directory, embeddings = create_vector_database(documents)
-        db = create_vector_database(documents)
-        #db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
-        
-        retriever = db.as_retriever( search_type="mmr", search_kwargs={"k": 8})
-        
-        callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-        llm=AzureChatOpenAI(deployment_name='chat', callback_manager=callback_manager, verbose=True)
-        
-        memory = ConversationSummaryMemory(llm=llm,memory_key="chat_history",return_messages=True)
-        qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
-    
-        query = st.text_input("Ask a question:")
-        if st.button("Get Answer"):
-            if query:
-                result = qa(query)
-                st.write(result['answer'])     
+    if data:
+        # Get column values
+        columns = data.columns.tolist()
+        return {"columns": columns, "data": data.to_dict(orient='records')}
+    else:
+        return {"error": "File not found or unable to read."}
