2ffe1fa0c84f755b21d23259d8301c5d9431dc2a
Vinay Naik <49183529+vinaynaik96@users.noreply.github.com>
2023-12-12 09:09:21 +0530

    Update vinny.py

--- vinny.py
+++ vinny.py
@@ -1,31 +1,5 @@
-import os
-import pandas as pd
-from fastapi import FastAPI
-
-app = FastAPI()
-
-def search_value_by_keyword(csv_path, keyword):
-    if os.path.exists(csv_path):
-        # Read the CSV file using pandas
-        df = pd.read_csv(csv_path)
-        
-        # Search for the keyword in the 'Keyword' column
-        filtered_data = df[df['Keyword'] == keyword]
-        
-        if not filtered_data.empty:
-            # Get the value corresponding to the keyword
-            return filtered_data['Value'].iloc[0]
-        else:
-            return None
-    else:
-        return None
-
-@app.get("/search/")
-async def search_value(keyword: str):
-    csv_file_path = "path_to_your_csv_file/data.csv"  # Specify your CSV file path here
-    result = search_value_by_keyword(csv_file_path, keyword)
-    
-    if result is not None:
-        return {"keyword": keyword, "value": result}
-    else:
-        return {"error": "Keyword not found or file not found."}
+[Document(page_content='Service Line: CIS\nSlno: 392\nBotNames: local admin rights for Desktop/laptop\nDescription: local admin rights for Desktop/laptop\nAccount Impl: RAC\nStatus: Deployed\nEffort savings: 0.4166666666666667\nTicketed/Non Ticketed: Ticketed\nCategory: Service Request\nSub category: Access Management\nTechnology: \nTool: MS SCORCH', metadata={'row': 5902, 'source': 'botstore.csv'}),
+ Document(page_content='Service Line: CIS\nSlno: 392\nBotNames: local admin rights for Desktop/laptop\nDescription: local admin rights for Desktop/laptop\nAccount Impl: RAC\nStatus: Deployed\nEffort savings: 0.4166666666666667\nTicketed/Non Ticketed: Ticketed\nCategory: Service Request\nSub category: Access Management\nTechnology: \nTool: MS SCORCH', metadata={'row': 5902, 'source': 'botstore.csv'}),
+ Document(page_content='Service Line: CIS\nSlno: 881\nBotNames: local admin rights for Desktop/laptop\nDescription: local admin rights for Desktop/laptop\nAccount Impl: Vorwerk\nStatus: Deployed\nEffort savings: 0.4166666666666667\nTicketed/Non Ticketed: \nCategory: Service Request\nSub category: Access Management\nTechnology: \nTool: MS SCORCH', metadata={'row': 6391, 'source': 'botstore.csv'}),
+ Document(page_content='Service Line: CIS\nSlno: 881\nBotNames: local admin rights for Desktop/laptop\nDescription: local admin rights for Desktop/laptop\nAccount Impl: Vorwerk\nStatus: Deployed\nEffort savings: 0.4166666666666667\nTicketed/Non Ticketed: \nCategory: Service Request\nSub category: Access Management\nTechnology: \nTool: MS SCORCH', metadata={'row': 6391, 'source': 'botstore.csv'}),
+ Document(page_content='Service Line: CIS\nSlno: 1433\nBotNames: myAccess - New hire/Internet\nDescription: myAccess - New hire/Internet\nAccount Impl: Kohler\nStatus: Deployed\nEffort savings: 0.05\nTicketed/Non Ticketed: \nCategory: Administration\nSub category: Access & Authorization\nTechnology: \nTool: CAC', metadata={'row': 6943, 'source': 'botstore.csv'})]
